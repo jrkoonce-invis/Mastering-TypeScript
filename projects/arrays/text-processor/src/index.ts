4add7df75b943e89a24dceb1061c38a5a0851726
Jameson Koonce <jamesonkoonce@gmail.com>
2024-10-29 13:20:22 -0500

    Projects 05 and 06

--- projects/arrays/text-processor/src/index.ts
+++ projects/arrays/text-processor/src/index.ts
@@ -1,2 +1,58 @@
 // Write your alignTexts function here! ✨
 // You'll need to export it so the tests can run it.
+
+export type AlignmentOptions = {
+	width: number;
+	align?: "left" | "middle" | "right";
+};
+
+export function alignTexts(texts: string[], options: AlignmentOptions) {
+	let OPTION = options.align ? options.align : "left";
+	let ans: string[][] = [];
+	let gi: number = 0;
+	for (let line of texts) {
+		let index: number = 0;
+		let words: string[] = line.split(" ");
+		let curr: string[] = [];
+
+		curr[index] = words[0];
+		let len: number = words[0].length;
+		for (let word of words.slice(1)) {
+			if (len + word.length + 1 <= options.width) {
+				// +1 for space btwn words
+				len += word.length + 1;
+				curr[index] += ` ${word}`;
+			} else {
+				// add spaces based on alignment
+				if (OPTION == "left") {
+					curr[index] += " ".repeat(options.width - len);
+				} else if (OPTION == "right") {
+					curr[index] = " ".repeat(options.width - len) + curr[index];
+				} else {
+					curr[index] =
+						" ".repeat((options.width - len) / 2) +
+						curr[index] +
+						" ".repeat((options.width - len) / 2);
+				}
+				index += 1;
+				len = word.length;
+				curr[index] = word;
+			}
+		}
+		if (curr[index].length < options.width) {
+			if (OPTION == "left") {
+				curr[index] += " ".repeat(options.width - len);
+			} else if (OPTION == "right") {
+				curr[index] = " ".repeat(options.width - len) + curr[index];
+			} else {
+				curr[index] =
+					" ".repeat(Math.floor((options.width - len) / 2)) +
+					curr[index] +
+					" ".repeat(Math.ceil((options.width - len) / 2));
+			}
+		}
+		ans[gi] = curr;
+		gi++;
+	}
+	return ans;
+}
--- projects/functions/structural-kitchen/src/index.ts
+++ projects/functions/structural-kitchen/src/index.ts
@@ -1,2 +1,72 @@
 // Write your createKitchen function here! ✨
 // You'll need to export it so the tests can run it.
+
+import { Ingredients } from "./solution";
+
+export type RecipeObj = {
+	succeeded: boolean;
+	newStock?: Stock;
+};
+
+export type Kitchen = {
+	announce: () => string;
+	clean: (time?: number) => void;
+	purchase: (expense: number) => boolean;
+	prepare: (recipe: (ingredients: Stock) => RecipeObj) => boolean;
+};
+
+export type Stock = {
+	breads: number;
+	fruits: number;
+	sauces: number;
+	vegetables: number;
+};
+
+export function createKitchen(
+	budget: number,
+	cleaner: (dirt: number, time?: number) => number,
+	supplier: (expense: number) => Stock
+): Kitchen {
+	let dirt = 0;
+	let stock: Stock = {
+		breads: 0,
+		fruits: 0,
+		sauces: 0,
+		vegetables: 0,
+	};
+
+	return {
+		announce() {
+			return `I have ${dirt} much dirt, ${budget} budget, ${stock.breads} bread(s), ${stock.fruits} fruit(s), ${stock.sauces} sauce(s), and ${stock.vegetables} vegetable(s).`;
+		},
+		clean(time?: number) {
+			dirt = cleaner(dirt, time);
+		},
+		purchase(expense: number): boolean {
+			if (expense <= budget) {
+				let ingredients = supplier(expense);
+				stock.breads += ingredients.breads;
+				stock.fruits += ingredients.fruits;
+				stock.sauces += ingredients.sauces;
+				stock.vegetables += ingredients.vegetables;
+
+				budget -= expense;
+
+				return true;
+			}
+			return false;
+		},
+		prepare(recipe: (ingredients: Stock) => RecipeObj): boolean {
+			if (dirt < 100) {
+				let rec = recipe(stock);
+				dirt += 1;
+
+				if (rec.succeeded && rec.newStock) {
+					stock = rec.newStock;
+				}
+				return rec.succeeded;
+			}
+			return false;
+		},
+	};
+}
