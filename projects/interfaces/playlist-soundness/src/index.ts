97eb9acbbf749291484b858f45ad230aa07354b1
Jameson Koonce <jamesonkoonce@gmail.com>
2024-10-30 16:35:01 -0500

    interfaces

--- projects/interfaces/playlist-soundness/src/index.ts
+++ projects/interfaces/playlist-soundness/src/index.ts
@@ -1,2 +1,22 @@
 // Write your unrollPlaylist function and types here! ✨
 // You'll need to export the function so the tests can run it.
+
+interface Artists {
+	[name: string]: string[];
+}
+
+interface PlaylistObject {
+	artists: Artists;
+	songs: string[];
+	time: number;
+}
+
+interface Album {}
+
+interface Song {
+	name: string;
+}
+
+type MusicItem = Song | Album | Playlist;
+
+export function unrollPlaylist(item: MusicItem[]): PlaylistObject | void {}
